aa0ef707b7f7e3c1048cb2014c4b0b1796335b53
leforeigner <albertcamusleforeigner@gmail.com>
2020-11-27 13:41:31 +0000

    first

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+    mount ActionCable.server => '/cable'
+    root "chatwindow#index"
+    resources :comments
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+end
